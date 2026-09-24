9202dd4c907403824d60573ee68d33df6db56f9c
Alvaro <acasmar2010@ieszaidinvergeles.org>
2026-09-24 14:43:11 +0200

    ejercicio5

--- TIPOS_NUMERICOS/ejercicio5.py
+++ TIPOS_NUMERICOS/ejercicio5.py
@@ -1,1 +1,7 @@
+horas = int (input ("cuantas horas trabajaste: "))
+print (horas)
 
+coste = int (input ("cual es el coste por hora?"))
+print (coste)
+
+print ("el coste por hora es de ",coste * horas)
